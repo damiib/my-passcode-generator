f141172eb64e5568008df21706d359055f932c77
Damali <brown@MacBook-Pro.local>
2021-07-08 12:38:59 -0700

    passwordgeneratorhomework

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ function writePassword() {
 
 }
 var choices = {};
-
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numericArr = [1,2,3,4,5,6,7,8,9,0]
+var specialChArr = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","`","~",";",":","'",""]
 function generatePassword() {
   var passwordLength =  parseInt(prompt("How long do want your  password to be? Must be between 8 and 128 character") )
   if (passwordLength <8) {
@@ -28,17 +31,33 @@ function generatePassword() {
   if (!lowerCase && !upperCase && !numeric && !specialCharacters ){
     alert ("please select at least one option") 
 
-
+    return
 
   }
 
-  var choiceSets = ["lowerCase","upperCase", "number","specialCharacters"]
-  var password = choiceSet (generatePassword);
-  function generatepassword () {
+
+  var choiceArr =[]
+  if (lowerCase) {
+    choiceArr.push(...lowerArr)    
+  }
+  if (upperCase) {
+    choiceArr.push(...upperArr) 
+  }
+  if(numeric){
+    choiceArr.push(...numericArr)
+  }
+if(specialCharacters){
+  choiceArr.push(...specialCharactersArr)
+}
+
+  var ratherpasscode = ""
+  for (let index = 0; index < passwordLength; index++) {
   
- }
-return
-      
+    var randomindex = Math.floor(Math.random()*choiceArr.length)
+    ratherpasscode = ratherpasscode + choiceArr[randomindex]
+  }
+
+return ratherpasscode
     }
     
 
@@ -49,7 +68,7 @@ return
 
   
  
-  
+generateBtn.addEventListener("click",writePassword)  
 
 
 
