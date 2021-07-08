0a16c98740ae2cd332522e1cb642e037c2144dc0
Damali <brown@MacBook-Pro.local>
2021-07-08 11:26:42 -0700

    passwordgenerator

--- script.js
+++ script.js
@@ -9,6 +9,47 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var choices = {};
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var passwordLength =  parseInt(prompt("How long do want your  password to be? Must be between 8 and 128 character") )
+  if (passwordLength <8) {
+    alert("password must be at least 8 characters") 
+    return
+  }
+  if (passwordLength >128) {
+    alert("password must be less than 128 characters")
+    return 
+  }
+  var lowerCase = confirm ("Do you want lower case letters in your password?")
+  var upperCase = confirm ("Do you want upper case letters in your password?")
+  var numeric = confirm ("Do you want numeric numbers in your password?")
+  var specialCharacters = confirm ("Do you want special characters in your password?")
+  if (!lowerCase && !upperCase && !numeric && !specialCharacters ){
+    alert ("please select at least one option") 
+
+
+
+  }
+
+  var choiceSets = ["lowerCase","upperCase", "number","specialCharacters"]
+  var password = choiceSet (generatePassword);
+  function generatepassword () {
+  
+ }
+return
+      
+    }
+    
+
+  
+ 
+
+  
+
+  
+ 
+  
+
+
+
